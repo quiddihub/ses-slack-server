ee35286025789fef5a156b8be58e661340b1bbc7
Charles <33097105+cnhtatham@users.noreply.github.com>
2019-01-15 13:55:55 +0000

    Create create_ses_ruleset.py

---
+++ create_ses_ruleset.py
@@ -0,0 +1,15 @@
+import boto3
+import local_settings
+
+def verify_domain(dommain):
+  
+
+if __name__ == '__main__':
+  
+  ses = boto3.client('ses')
+  
+  response = client.create_receipt_rule_set(
+    RuleSetName='ses-slack'
+  )
+  
+  
